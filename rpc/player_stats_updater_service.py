17665359eecada1d2dfa2d10573731682aef62c0
Callum Winder <69708064+Calluumm@users.noreply.github.com>
2026-07-09 11:49:51 +0100

    relogic

--- rpc/player_stats_updater_service.py
+++ rpc/player_stats_updater_service.py
@@ -12,18 +12,45 @@ class PlayerStatsUpdater(player_stats_update_pb2_grpc.PlayerStatsUpdater):
         request: player_stats_update_pb2.Request,
         context: grpc.aio.ServicerContext,
     ) -> player_stats_update_pb2.Response:
-        search_players, old_membership, prev_warcounts, old_global_stats = await PlayerStatsTask.get_stats_track_references(needs_player_list=False, force_player_list=request.player_uuid)
-        inserts_war_update, inserts_war_deltas, inserts_guild_log, inserts, uuid_name, update_player_global_stats, deltas_player_global_stats = PlayerStatsTask.get_empty_stats_track_buffers()
+        _, old_membership, prev_warcounts, prev_graidcounts, old_global_stats = await PlayerStatsTask.get_stats_track_references(
+            needs_player_list=False,
+            force_player_list=request.player_uuid,
+        )
+        inserts_war_update, inserts_war_deltas, inserts_graid_update, inserts_graid_deltas, inserts_guild_log, inserts, uuid_name, update_player_global_stats, deltas_player_global_stats = PlayerStatsTask.get_empty_stats_track_buffers()
         failures = []
         for player_uuid in request.player_uuid:
-            res = await PlayerStatsTask.track_player(player_uuid, old_membership, prev_warcounts, old_global_stats,
-                                                    inserts_war_update, inserts_war_deltas, inserts_guild_log, inserts, uuid_name, update_player_global_stats, deltas_player_global_stats)
+            res = await PlayerStatsTask.track_player(
+                player_uuid,
+                old_membership,
+                prev_warcounts,
+                prev_graidcounts,
+                old_global_stats,
+                inserts_war_update,
+                inserts_war_deltas,
+                inserts_graid_update,
+                inserts_graid_deltas,
+                inserts_guild_log,
+                inserts,
+                uuid_name,
+                update_player_global_stats,
+                deltas_player_global_stats,
+            )
             if not res:
                 failures.append(player_uuid)
             
             await asyncio.sleep(1) # make this one slower
 
-        PlayerStatsTask.write_results_to_db(inserts_war_update, inserts_war_deltas, inserts_guild_log, inserts, uuid_name, update_player_global_stats, deltas_player_global_stats )
+        PlayerStatsTask.write_results_to_db(
+            inserts_war_update,
+            inserts_war_deltas,
+            inserts_graid_update,
+            inserts_graid_deltas,
+            inserts_guild_log,
+            inserts,
+            uuid_name,
+            update_player_global_stats,
+            deltas_player_global_stats,
+        )
         return player_stats_update_pb2.Response(failures=failures)
     
 
